7528580295daee036dd3f14ec79248a9e283b4f1
Alexander Rohrig <a.rohrig@me.com>
2020-10-06 12:34:38 -0700

    Initial Commit

---
+++ kassel/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  kassel
+//
+//  Created by Alexander Rohrig on 10/6/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
