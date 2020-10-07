9b4f5fcfb02a9cc591c41eae55a486bfe7202f39
Alexander Rohrig <a.rohrig@me.com>
2020-10-07 07:01:03 -0700

    version 2

--- kassel/ViewController.swift
+++ kassel/ViewController.swift
@@ -10,35 +10,45 @@ import Cocoa
 class ViewController: NSViewController {
 
     @IBOutlet weak var aField: NSTextField!
+    @IBOutlet weak var bField: NSTextField!
+    @IBOutlet weak var cField: NSTextField!
+    @IBOutlet weak var dField: NSTextField!
     @IBOutlet weak var nField: NSTextField!
     @IBOutlet weak var sumField: NSTextField!
     @IBOutlet weak var calButton: NSButton!
     
     var sum = 0
     var a = 0
+    var b = 0
+    var c = 0
+    var d = 0
     var n = 0
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
 
     @IBAction func calPressed(_ sender: Any) {
-        a = Int(aField.stringValue) ?? 0
         n = Int(nField.stringValue) ?? 0
+        a = 0
+        b = 0
+        c = 0
+        d = 0
         sum = 0
         
         for x in 1...n {
-            sum = sum + (a * x)
-            print(sum)
+            a = a + (2 * x)
+            b = b + (3 * x)
+            c = c + (4 * x)
+            d = d + (5 * x)
         }
+        
+        b = a + b
+        c = a + b + c
+        d = a + b + c + d
+        sum = a + b + c + d
+        
+        aField.stringValue = "\(a)"
+        bField.stringValue = "\(b)"
+        cField.stringValue = "\(c)"
+        dField.stringValue = "\(d)"
+        sumField.stringValue = "\(sum)"
         
         print(sum)
         sumField.stringValue = "\(sum)"
